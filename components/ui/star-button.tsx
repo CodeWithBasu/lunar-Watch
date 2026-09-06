2e6581701daffe5d7a4ca200e30db2427f841d0c
Basudev Moharana <basudevmuna111@gmail.com>
2026-09-06 23:12:18 +0530

    fix(ui): apply hover animations to checked state for mobile star button support

--- components/ui/star-button.tsx
+++ components/ui/star-button.tsx
@@ -52,7 +52,7 @@ export const StarButton = ({ text = "WISHLIST", initialCount = 99 }: StarButtonP
           {/* Shine Sweep Effect */}
           <div className="absolute top-0 bottom-0 left-0 w-[200%] h-full bg-gradient-to-r from-transparent via-yellow-300/40 to-transparent animate-shine pointer-events-none z-0" />
           
-          <div className="z-10 transition group-hover:translate-x-4 peer-checked:group-hover:text-black">
+          <div className="z-10 transition group-hover:translate-x-4 peer-checked:translate-x-4 peer-checked:text-black group-hover:text-black">
             {text}
           </div>
           <svg
@@ -60,7 +60,7 @@ export const StarButton = ({ text = "WISHLIST", initialCount = 99 }: StarButtonP
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className="size-6 transition duration-500 group-hover:scale-[1500%] group-hover:-translate-x-10"
+            className="size-6 transition duration-500 group-hover:scale-[1500%] group-hover:-translate-x-10 peer-checked:scale-[1500%] peer-checked:-translate-x-10"
           >
             <path
               d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
